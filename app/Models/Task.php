047c19b9941d4b46a6b35dae0bc6b079235f1540
Adnane-elKanbouji <adnane1234598@gmail.com>
2024-09-23 09:10:40 +0100

    first commit

---
+++ app/Models/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    public function category(){
+        return $this->belongsTo(Category::class,'category_id');
+    }
+    public function getCreatedAtAttribute($value){
+        return Carbon::parse($value)->diffForHumans();
+    }
+}
---
+++ resources/js/Pages/Task/Index.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container">
+        <div class="row my-5">
+           <div class="col-md-9">
+            <div class="card">
+                <div class="card-body">
+<table class="table">
+ <thead>
+    <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Body</th>
+        <th>Category</th>
+        <th>Done</th>
+        <th>Created</th>
+    </tr>
+ </thead>
+ <tbody>
+    {{ console.log(tasks)}}
+    <tr v-for="task in tasks.data" :key="task.id">
+        <td> {{ task.id }}</td>
+        <td> {{ task.title }}</td>
+        <td> {{ task.body }}</td>
+        <td> {{ task.category.name }} </td>
+        <td>
+            <div v-if="task.done">
+               <span class="badge bg-success">
+                  Done
+               </span>
+            </div>
+            <div v-else>
+              <span class="badge bg-danger">
+                   Processing ...
+              </span>
+            </div>
+        </td>
+        <td>{{ task.created_at }}</td>
+        <td></td>
+    </tr>
+ </tbody>
+</table>
+                </div>
+            </div>
+        
+           </div>
+           <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    
+                </div>
+            </div>
+           </div>
+        </div>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    tasks: {
+        type: Object,
+        required: true,
+    },
+    categories: {
+        type: Array,
+        required: true,
+    }
+})
+
+
+
+</script>
